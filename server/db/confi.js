a4ac2e2f90e8cf49156fe75ec90b2beceb1e53d7
AYUSH SOLANKI <arushsolanki1@gmail.com>
2025-07-18 14:00:46 +0530

    Update Origin

--- server/db/confi.js
+++ server/db/confi.js
@@ -1,3 +1,4 @@
 const mongoose=require('mongoose');
 const url='mongodb+srv://b:b@b.e4hei.mongodb.net/?retryWrites=true&w=majority&appName=b'
 mongoose.connect(url);
+console.log("data");
--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(cors({
-  origin: "http://localhost:3002",
+  origin: "http://localhost:3000",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
